61262995e3e41564a3e1ec0c6e6dd1ffc82cd885
Steven Shave <stevenshave@gmail.com>
2023-02-16 08:26:15 +0000

    Incrememented documentation version number

--- docsource/conf.py
+++ docsource/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Steven Shave'
 author = 'Steven Shave'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.4'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
